5a06bf4bad3ac800de630291f99aaf3e4989b0db
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2026-02-06 16:04:54 +0000

    feat: add hello world (again)

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,5 +3,5 @@ import { createFileRoute } from '@tanstack/react-router'
 export const Route = createFileRoute('/')({ component: App })
 
 function App() {
-  return <h1>Hello World</h1>
+  return <h1>Hello World (Again)</h1>
 }
